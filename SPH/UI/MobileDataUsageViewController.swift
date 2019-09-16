6618e5129d52eedf204ffa3cce46b193470b6f15
muhammad.shauket <muhammad.shauket@rakuten.com>
2019-09-16 16:25:46 +0800

    Added empty group for project architecture, also added controller with name MobileDataUsageviewController

---
+++ SPH/UI/MobileDataUsageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MobileDataUsageViewController.swift
+//  SPH
+//
+//  Created by Muhammad, Shauket | RASIA on 16/9/19.
+//  Copyright © 2019 Muhammad, Shauket | RASIA. All rights reserved.
+//
+
+import UIKit
+
+class MobileDataUsageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
